4f067ad1f3f8a01dd281912f0d550742375f9299
bishalbhowmik <77568117+bishalbhowmik@users.noreply.github.com>
2023-07-29 17:50:13 +0600

    Project & Work

---
+++ src/components/data/data.js
@@ -0,0 +1,21 @@
+import doctor from '../assets/projects/doctor-portal.png';
+import shopping from '../assets/projects/shopping.png';
+
+export const data=[
+    {
+        id:1,
+        name:"MERN Stack Application",
+        image:doctor,
+        github:"https://github.com/bishalbhowmik/doctor-portal-frontend",
+        live:"https://doctor-portal-ce5ca.firebaseapp.com/",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:shopping,
+        github:"https://github.com/bishalbhowmik/shopping-cart",
+        live:"https://dazzling-peony-0d6139.netlify.app/",
+    },
+
+
+]
